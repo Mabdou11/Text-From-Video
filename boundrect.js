36f01d86cc52bedf287799304e3c52509ec1418b
Mabdou11 <menouerabdou@gmail.com>
2019-10-31 07:45:07 +0100

    Added rectangle selection, pre loading

--- boundrect.js
+++ boundrect.js
@@ -1,33 +1,104 @@
-var canvas = document.getElementById("myCanvas");
+
+var canvas = document.createElement("canvas");
+canvas.setAttribute("id", "myCanvas");
+
+
+$(document).ready(function(){
+
+});
+
+document.body.appendChild(canvas);
 var ctx = canvas.getContext("2d");
 var mdX,mdY,mvX,mvY; // mouse coordinates
 var started = false;
+var scale;
 
-let click = e =>{
-	if(!started){
-			mdX = e.pageX-10;
-			mdY = e.pageY-10;
-			started = true;
-			ctx.beginPath();
+function bound(){
+
+	var x = (mdX+mvX);
+	var y = (mdY+mvY);
+	return{
+		X:scale()*Math.min(mdX,x),
+		Y:scale()*Math.min(mdY,y),
+		W:scale()*(Math.max(mdX,x)-Math.min(mdX,x)),
+		H:scale()*(Math.max(mdY,y)-Math.min(mdY,y))
+	};
+}
+
+
+$(document).ready(function(){
+	function pos(){
+		if($("video").position(){
+			return {
+				top:$("video").position().top,
+				left:$("video").position().left,
+				right:$("video").position().right
+			}	
+		}else{
+			return {
+				top:-9999,
+				left:-9999,
+				right:-99999
+			}
 		}
-};
-let move = e =>{
-	if(started){
-		mvX = e.pageX-mdX-10;
-		mvY = e.pageY-mdY-10;
-		ctx.beginPath();
-		ctx.rect(mdX, mdY, mvX, mvY);
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.stroke();
 	}
-};
 
-document.addEventListener("mousedown",click);
-document.addEventListener("mousemove",move);
-document.addEventListener("mouseup", () =>{
-	started = false;
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	X = (mdX+mvX);
-	Y = (mdY+mvY);
-	document.getElementById("bounds").innerHTML = "X:"+Math.min(mdX,X)+", "+Math.max(mdX,X)+" Y:"+Math.min(mdY,Y)+", "+Math.max(mdY,Y);
+	var video = document.querySelector('video');
+	var height = $("video").height();
+	var width = $("video").width();
+
+	scale = ()=> {
+		return video.videoHeight/$("video").height();
+	}
+
+	$("#myCanvas").css({
+		position: 'absolute',
+		'z-index': -1,
+		top: pos().top+'px',
+		left: pos().left+'px',
+	});
+	$("#myCanvas").attr("height", height);
+	$("#myCanvas").attr("width", width);
+
+
+	let click = e =>{
+		if(!started){
+
+				mdX = e.pageX - pos().left;
+				mdY = e.pageY - pos().top;
+				started = true;
+				ctx.beginPath();
+			}
+	};
+
+	let move = e =>{
+			if(started){
+				$("#myCanvas").css({
+					'z-index': 990
+				});
+
+				mvX = e.pageX-mdX-pos().left;
+				mvY = e.pageY-mdY-pos().top;
+				ctx.beginPath();
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillStyle = "#51E7FF27";
+				ctx.fillRect(mdX, mdY, mvX, mvY);
+				ctx.strokeStyle = "#51E7FF"
+				ctx.lineWidth = 2;
+				ctx.rect(mdX, mdY, mvX, mvY);
+				ctx.stroke();
+//	document.getElementById("bounds").innerHTML = "X:"+bound().X+" --> "+(bound().X+bound().W)+" |  Y:"+bound().Y+" --> "+(bound().Y+bound().H);
+			}
+		};
+
+	document.addEventListener("mousedown",click);
+	document.addEventListener("mousemove",move);
+	document.addEventListener("mouseup", () =>{
+		$("#myCanvas").css({
+			'z-index': -1
+		});
+		started = false;
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	});
+
 });
--- content.js
+++ content.js
@@ -12,55 +12,64 @@ const copyToClipboard = str => {
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 document.addEventListener("keydown", function(evt){
-			'use strict';
-	if(evt.altKey && evt.shitfKey){
+	if(evt.altKey && evt.shiftKey){
 		$(document).ready(function(){
-			alert("yohoho!");
+			alert("yoohoo!");
 		});
 	}
 });
 });
 
+function coldStart(){
+	//pre execute tesseract on a small blank image:
+		var blank10x10 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII="
+		Tesseract.recognize(blank10x10).progress((progress)=>{console.log("init Tesseract")}).then(()=>{console.log("init Complete")});
+	}
+
 $(document).ready(function(){
 	// instant.page == preload a page faster.
 //	$('body').append('<script src="//instant.page/2.0.1" type="module" defer integrity="sha384-4Duao6N1ACKAViTLji8I/8e8H5Po/i/04h4rS5f9fQD6bXBBZhqv5am3/Bf/xalr"></script>');
 
-/*
-	// preload tesseract for faster interaction
-	let blankimg = new Image();
-	blankimg = "data:image/png;base64,R0lGODlhAQABAIAAAP7//wAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==";
-	Tesseract.recognize(blankimg).progress().then((result)=>{
-		$('#description').children("yt-formatted-string").prepend("Tesseract preloaded!\n")
-	});
-*/
+if($("video").position()){
+// for now, it does not work with non <video> elements	
+	coldStart();
 
 	$(document).on("keydown",function(kevent){
 	if (kevent.altKey && kevent.ctrlKey ) {
 	//	$(document).on('click','video', function(){
 		'use strict';
-
 		(function(window, document) {
  			//was a blank div for mouse move result
 			$('body').append('<div id="result" style=" font-family:\'Roboto\', sans-serif; z-index:-1; position:absolute; background-color:#eee; border-color: #333; border-radius:10px 10px 10px 10px; transition:transform 0.3s cubic-bezier(.02,1.23,.79,1.08);">Doing Stuff, please wait till this disappears<br>this will become a notification or something</div>');			
 
 		    let canvas = document.createElement('canvas');
 		    var video = document.querySelector('video');
+		    var X = 0;
+		    var Y = 0;
+		    var W = parseInt(video.videoWidth);
+		    var H = parseInt(video.videoHeight);
 
-		    canvas.width = parseInt(video.videoWidth);
-		    canvas.height = parseInt(video.videoHeight);
-		    
+		    if(bound().W){
+		    	X = bound().X;
+		    	Y = bound().Y;
+		    	W = bound().W;
+		    	H = bound().H;
+		    }
+
+		    canvas.width = parseInt(W);
+		    canvas.height = parseInt(H);
 		    var ctx = canvas.getContext('2d');
-		    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+		    ctx.drawImage(video, X, Y, W, H, 0, 0, canvas.width, canvas.height);
 		    var base64ImageData = canvas.toDataURL('image/png');
 
+//		    $('#capture').attr('src',base64ImageData);
 
 			Tesseract.recognize(base64ImageData).progress((progress)=>{
 				
-				console.log(status);
 		 		if(progress.status != 'recognizing text'){
 		 		
 		 			// show progress on mouse cursor
-		 		$(document).on('mousemove', function(e){
+		 		$(document).on('mousemove', function(e){			
 		 			$("#result").css({
  					left: (e.pageX+20)+'px',
  					top : (e.pageY+5)+'px',
@@ -79,20 +88,20 @@ $(document).ready(function(){
 						}else $('#result').text('There you go!');
 					}
 				}
-								
+		
 		 	}).then((result)=>{
 		 		$('body').children('#result').text(result.text);
 		 		copyToClipboard(result.text);
-		 		setTimeout(2000,()=>{
-		 			$("#result").remove();
-		 		});
-		 		
-		 	});
-		
+		 		setTimeout(function(){
+		 		$('#result').remove();
+		 		},2000);
+		 	
+		 	});	
 
 		})(window, document);
 	//	});
 	}
 	});
+}
 });
 
